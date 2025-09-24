bea4a6edb46e13bb16a7225d53298b1a72dd2804
Gosha Pogrebitskiy <goshik200712@gmail.com>
2025-09-24 14:56:40 +0300

    solved task a, ver 1.0

--- a.cpp
+++ a.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
 
-int main( int argc, char** argv ) {
-    std::cout << "Hello world";
+int Sort( int *A, int N ) {
+    int count = 0;
+
+    for (int i = 1; i < N; i++) {
+        int mem = A[i];
+        
+        int j = i;
+        
+        while (j > 0) {
+            count++;
+            if (mem < A[j - 1]) {
+                A[j] = A[j - 1];               
+            } else {
+                break;
+            }
+            j--;
+        }
+
+        A[j] = mem;
+        
+        /*
+        // Output for debugging
+        for (int k = 0; k < N; k++)
+            std::cout << A[k] << " ";
+        std::cout << ": inserting " << mem << ", count = " << count << std::endl;
+        */
+    }
+    
+    return count;
+}
+
+int main( int argc, char **argv ) {
+    int A[100];
+    int N;
+
+    std::cin >> N;
+    for (int i = 0; i < N; i++) {
+        std::cin >> A[i];
+    }
+
+    std::cout << Sort(A, N);
+
     return 0;
 }
