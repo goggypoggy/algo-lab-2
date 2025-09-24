30c731a2e0c55b2da0a18e6a27a8089935a97931
Georgiy Pogrebitskiy <goshik200712@gmail.com>
2025-09-24 23:55:45 +0300

    finished quick sort with verbose debug output

--- b.cpp
+++ b.cpp
@@ -1,75 +1,10 @@
-#include <cmath>
-#include <cstring>
 #include <iostream>
 
-/* -- DEBUG FUNCTIONS AND VARIABLES -- */
-int kHeapOutTabSize = 1;
-
-void NumOut( int x, int desired_size ) {
-    int actual_size = 1;
-    if (abs(x) >= 10) {
-        actual_size = std::ceil(std::log10(x));
-    }
-
-    std::cout << x;
-    if (actual_size < desired_size) {
-        for (int i = 0; i < desired_size - actual_size; i++) {
-            std::cout << ".";
-        }
+void OutputArray( int *A, int N ) {
+    for (int i = 0; i < N; i++) {
+        std::cout << A[i] << (i == N - 1 ? "\n" : " ");
     }
 }
-
-void TabOut( int desired_size ) {
-    for (int i = 0; i < desired_size; i++) {
-        std::cout << " ";
-    }
-}
-
-void HeapOut( int *heap, int N ) {
-    int max_level = std::ceil(std::log2(N));
-
-    int ind = 0;
-    for (int l = max_level; l > 0; l--) {
-        std::cout << max_level - l << ": ";
-        
-        TabOut(kHeapOutTabSize * (std::pow(2, l - 1) - 1));
-        
-        for (int k = 0; k < std::pow(2, max_level - l); k++) {
-            NumOut(heap[ind], kHeapOutTabSize);
-            TabOut(kHeapOutTabSize * (std::pow(2, l) - 1));
-            ind++;
-            if (ind >= N) {
-                break;
-            }
-        }
-
-        std::cout << std::endl;
-    }
-}
-
-int ConvertStrToInt( char *str ) {
-    int result = 0;
-
-    int max_pow10 = 0;
-    while (str[max_pow10] != 0) {
-        max_pow10++;
-    }
-
-    for (int i = 0; i < max_pow10; i++) {
-        result += (str[i] - '0') * std::pow(10, max_pow10 - 1 - i);
-    }
-
-    return result;
-}
-
-void ParseArgs( int argc, char **argv ) {
-    for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i - 1], "--tab-size") == 0) {
-            kHeapOutTabSize = ConvertStrToInt(argv[i]);
-        }
-    }
-}
-/* -- END OF DEBUG FUNCS AND VARS -- */
 
 void Swap( int *a, int *b ) {
     int tmp = *a;
@@ -77,62 +12,58 @@ void Swap( int *a, int *b ) {
     *b = tmp;
 }
 
-void SiftDown( int i, int *heap, int heap_size ) {
-    int left = 2 * i + 1;
-    int right = 2 * i + 2;
+int allowed_steps = 5;
 
-    if (left >= heap_size) {
+// [l; r)
+void Partition( int l, int r, int *A ) {
+    if (r - l < 2) {
         return;
     }
 
-    int j = left;
-    if (right < heap_size && heap[right] > heap[left]) {
-        j = right;
+    std::cout << "/\"\" [" << l << "; " << r << ") partition \"\"\\\n";
+    std::cout << "Index:  ";
+    for (int i = l; i < r; i++) {
+        std::cout << i << (i == r - 1 ? "\n" : " ");
+    }
+    std::cout << "Start:  ";
+    OutputArray(A + l, r - l);
+
+    int pivot = A[(l + r) / 2];
+    int i = l;
+    int j = r - 1;
+    
+    std::cout << "Pivot:  A[" << (l + r) / 2 << "] = " << pivot << std::endl;
+    
+    while (i <= j) {
+        while (A[i] < pivot && i < r) {
+            i++;
+        }
+
+        while (A[j] > pivot && j >= l) {
+            j--;
+        }
+
+        if (i <= j) {
+            Swap(A + i, A + j);
+            i++;
+            j--;
+        }
     }
 
-    if (heap[i] < heap[j]) {
-        Swap(heap + i, heap + j);
-    } else {
-        return;
-    }
+    std::cout << "Result: ";
+    OutputArray(A + l, r - l);
+    std::cout << "i = " << i << ", j = " << j << std::endl;
+    std::cout << "\\__ [" << l << "; " << r << ") partition __/\n\n";
 
-    SiftDown(j, heap, heap_size);
+    Partition(l, j + 1, A);
+    Partition(i, r, A);
 }
 
-void BuildHeap( int *heap, int heap_size ) {
-    for (int i = heap_size / 2 - 1; i >= 0; i--) {
-        SiftDown(i, heap, heap_size);
-        std::cout << "After SiftDown-ing " << i << ":\n";
-        HeapOut(heap, heap_size);
-    }
-}
-
-void BuildSortedArray( int *heap, int heap_size ) {
-    for (int i = heap_size - 1; i >= 0; i--) {
-        Swap(heap + 0, heap + heap_size - 1);
-        heap_size--;
-        SiftDown(0, heap, heap_size);
-    }
-}
-
-void HeapSort( int *A, int N ) {
-    std::cout << "Unbuilt heap:\n";
-    HeapOut(A, N);
-
-    BuildHeap(A, N);
-
-    std::cout << "Built heap:\n";
-    HeapOut(A, N);
-
-    BuildSortedArray(A, N);
-
-    std::cout << "Sorted array:\n";
-    HeapOut(A, N);
+void QuickSort( int *A, int N ) {
+    Partition(0, N, A);
 }
 
 int main( int argc, char **argv ) {
-    ParseArgs(argc, argv);
-    
     int A[100000] = {0};
     int N;
 
@@ -141,7 +72,8 @@ int main( int argc, char **argv ) {
         std::cin >> A[i];
     }
 
-    HeapSort(A, N);
+    QuickSort(A, N);
+    OutputArray(A, N);
 
     return 0;
 }
