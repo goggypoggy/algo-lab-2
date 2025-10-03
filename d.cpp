4a5fc51b3eab22ab430692e494a1f57d7bb8b374
Georgiy Pogrebitskiy <goshik200712@gmail.com>
2025-10-03 19:46:42 +0300

    task d done, 100/100, ver 1.0

--- d.cpp
+++ d.cpp
@@ -13,6 +13,52 @@ uint32_t nextRand24(uint32_t a, uint32_t b) {
     return cur >> 8;  // берём старшие 24 бита (сдвиг вправо на 8)
 }
 
+uint32_t extra[MAXN];
+
+// [l; r)
+uint64_t SpecialMerge( int l, int r ) {
+    if (r - l <= 1) {
+        return 0;
+    }
+    
+    int mid = (l + r) / 2;
+    uint64_t count = 0;
+
+    count += SpecialMerge(l, mid);
+    count += SpecialMerge(mid, r);
+
+    int i = l;
+    int j = mid;
+    int k = l;
+
+    while (k < r) {
+        if (i < mid && j < r) {
+            if (arr[i] <= arr[j]) {
+                extra[k] = arr[i];
+                i++;
+            } else {
+                extra[k] = arr[j];
+                j++;
+                count += mid - i; // amount of elements in left half
+            }
+        } else if (i < mid) {
+            extra[k] = arr[i];
+            i++;
+        } else if (j < r) {
+            extra[k] = arr[j];
+            j++;
+        }
+
+        k++;
+    }
+    
+    for (int a = l; a < r; a++) {
+        arr[a] = extra[a];
+    }
+    
+    return count;
+}
+
 int main( int argc, char **argv ) {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
@@ -28,7 +74,7 @@ int main( int argc, char **argv ) {
 
     // Далее используйте arr[0..n-1] для подсчёта инверсий
     
-    
+    std::cout << SpecialMerge(0, n);
 
     return 0;
 }
