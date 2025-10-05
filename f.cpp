9b7b5ed3f53fba87221ca4c3ff6b1859d75f107b
Georgiy Pogrebitskiy <goshik200712@gmail.com>
2025-10-05 16:47:54 +0300

    task f done, 100/100

---
+++ f.cpp
@@ -0,0 +1,102 @@
+#include <cstdint>
+#include <iostream>
+
+const int kMaxN = 10'000'000;
+
+uint32_t cur = 0;  // беззнаковое 32-битное число
+
+// Генератор 24-битного числа
+uint32_t nextRand24(uint32_t a, uint32_t b) {
+    cur = cur * a + b;      // вычисляется с переполнениями по модулю 2^32
+    return cur >> 8;         // число от 0 до 2^24 - 1
+}
+
+// Генератор 32-битного числа на основе двух 24-битных
+uint32_t nextRand32(uint32_t a, uint32_t b) {
+    uint32_t x = nextRand24(a, b);
+    uint32_t y = nextRand24(a, b);
+    return (x << 8) ^ y;     // число от 0 до 2^32 - 1
+}
+
+#define RAND_MAX INT_MAX
+
+// [min; max)
+int Rand( int min, int max ) {
+    return std::rand() % (max - min) + min;
+}
+
+void Swap(uint32_t *a, uint32_t *b) {
+    uint32_t temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int Partition(uint32_t *arr, int l, int r) {
+    uint32_t pivot = arr[Rand(l, r)];
+
+    int i = l;
+    int j = r - 1;
+
+    while (i <= j) {        
+        while (i < r && arr[i] < pivot) {
+            i++;
+        }
+        
+        while (j >= l && arr[j] > pivot) {
+            j--;
+        }
+
+        if (i <= j) {
+            Swap(arr + i, arr + j);
+            i++;
+            j--;
+        }
+    }
+    
+    return i;
+}
+
+// [l; r)
+uint32_t QuickSearch(uint32_t *arr, int l, int r, int k) {
+    if (l == r - 1) {
+        return arr[l];
+    }
+
+    int mid = Partition(arr, l, r);
+
+    if (k < mid) {
+        return QuickSearch(arr, l, mid, k);
+    } else {
+        return QuickSearch(arr, mid, r, k);
+    }
+}
+
+uint64_t CountSum(  uint32_t x, uint32_t *arr, int n ) {
+    uint64_t result = 0;
+
+    for (int i = 0; i < n; i++) {
+        int64_t a = std::abs((int64_t)x - (int64_t)arr[i]);
+        result += (uint64_t)a;
+    }
+
+    return result;
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    uint32_t n, a, b;
+    std::cin >> n >> a >> b;
+
+    uint32_t *arr = new uint32_t[n];
+
+    for (uint32_t i = 0; i < n; i++) {
+        arr[i] = nextRand32(a, b);  // генерируем i-й элемент
+    }
+
+    std::cout << CountSum(QuickSearch(arr, 0, n, n / 2), arr, n);
+    
+    delete[] arr;
+    return 0;
+}
